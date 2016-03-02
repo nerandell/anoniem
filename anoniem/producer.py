4de8ce45a355800afd41b8266d521dbc35d647f8
Ankit Chandawala <ankit.chandawala@kayako.com>
2016-03-02 15:36:37 +0530

    Fix method name

--- anoniem/producer.py
+++ anoniem/producer.py
@@ -14,6 +14,6 @@ class Producer:
     def create_job(self, table, primary_key, column, action):
         primary_keys = self._cache[table]
         random_data = getattr(self._provider, action)
-        print random_data.__class.__name__
+        print random_data.__class__.__name__
         job = [(table, column, primary_key, random_data(), row) for row in primary_keys]
         return job
